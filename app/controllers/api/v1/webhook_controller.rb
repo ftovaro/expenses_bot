5a75f150177e5c8bd6bcda01c4b7dd708fa4be9f
Felipe Tovar <f.tovar12@gmail.com>
2024-07-19 21:53:24 -0500

    Organize webhook verify params

--- app/controllers/api/v1/webhook_controller.rb
+++ app/controllers/api/v1/webhook_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::WebhookController < ApiController
   def verify
     token = 'HAPPY'
-    if params.dig(:hub, :verify_token) == token
-      return render json: { hub: { challenge: params[:hub][:challenge] } }, status: :ok
+    if params.dig('hub.verify_token') == token
+      return render json: { 'hub.challenge': params.dig('hub.challenge') }, status: :ok
     end
 
     render json: { message: 'Verification failed' }, status: :forbidden
